11f6ac2048b0012e7b7b14dc66db6d9016f4cef9
Diego-Chirico <diegochiricofullstack@gmail.com>
2021-06-25 10:23:22 -0300

    atualizacao css

--- src/Pagina404/Pagina404.jsx
+++ src/Pagina404/Pagina404.jsx
@@ -2,7 +2,7 @@ import './css/P404.css';
 const Pagina404 = () => {
     return ( 
         <div className="fundo">
-            <h1>404 error page</h1>
+            <h1 classname="titulo01">A pagina não foi encontrada</h1>
 
   </div>
      );
